d8ae897564591a5433069e12476d31a3edd5489b
SunnyZhang0916 <87867258+SunnyZhang0916@users.noreply.github.com>
2022-08-08 17:47:22 +0200

    用 equals() 比较两个对象的实际内容, 前提是要在自己的新类中覆盖equals()方法

--- EqualsMethod.java
+++ EqualsMethod.java
@@ -1,7 +1,7 @@
 public class EqualsMethod {
 
     /*
-    equals() 才是比较对象的实际内容，但不是绝对的。这里的做法是在自己的新类中覆盖equals（）方法。
+    equals() 才是比较对象的实际内容，但不是绝对的。这里的做法是在自己的新类中覆盖equals（）方法
      */
     public static void main(String[] args){
         Integer n1 = new Integer(47);
